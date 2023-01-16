d4af79eb9f095719b1385b009b98c7c715074dc6
Raivan <muhamadrvnptr@gmail.com>
2023-01-16 16:35:25 +0700

    Database Baru

--- database/eas_db.sql
+++ database/eas_db.sql
@@ -1,15 +1,21 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.3
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 09, 2022 at 08:42 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.5
+-- Waktu pembuatan: 16 Jan 2023 pada 10.33
+-- Versi server: 10.4.24-MariaDB
+-- Versi PHP: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `eas_db`
@@ -18,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `attendance`
+-- Struktur dari tabel `attendance`
 --
 
 CREATE TABLE `attendance` (
@@ -38,35 +44,19 @@ CREATE TABLE `attendance` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `attendance`
+-- Dumping data untuk tabel `attendance`
 --
 
 INSERT INTO `attendance` (`id`, `username`, `employee_id`, `department_id`, `shift_id`, `location_id`, `in_time`, `notes`, `image`, `lack_of`, `in_status`, `out_time`, `out_status`) VALUES
-(45, 'ADM011', 011, 'ADM', 1, 1, 1589178316, 'sdf', 'item-200511-8f5d7be1a1.jpg', 'None', 'Late', 1589178477, 'Early'),
-(48, 'ADM011', 011, 'ADM', 1, 1, 1589381121, '', 'item-200513-ad6953a07e.jpg', 'Notes', 'Late', 1589381127, 'Over Time'),
-(49, 'PCD010', 010, 'PCD', 2, 1, 1589384432, 'asdasd', '', 'None,image', 'Late', 1589384514, 'Over Time'),
-(50, 'ADM011', 011, 'ADM', 1, 1, 1589391038, '', '', 'Notes,image', 'On Time', 1589391056, 'Early'),
-(51, 'PCD010', 010, 'PCD', 3, 1, 1622553388, 'testing', 'item-210601-3946bb00df.png', 'None', 'Late', 1622553470, 'Over Time'),
-(52, 'PCD010', 010, 'PCD', 3, 2, 1631893356, 'none', '', 'None,image', 'Late', 1631893413, 'Over Time'),
-(53, 'STD026', 026, 'STD', 1, 1, 1631894335, 'none', '', 'None,image', 'Late', 1631894403, 'Over Time'),
-(54, 'ADM011', 011, 'ADM', 1, 2, 1631894692, 'demo', '', 'None,image', 'Late', 1631894696, 'Over Time'),
-(55, 'QCD027', 027, 'QCD', 6, 2, 1631499386, 'none..', '', 'None,image', 'Late', 1631529057, 'Early'),
-(56, 'QCD027', 027, 'QCD', 6, 2, 1631583036, 'none', '', 'None,image', 'Late', 1631611849, 'Early'),
-(58, 'QCD027', 027, 'QCD', 6, 1, 1631733350, 'none', '', 'None,image', 'Late', 1631797356, 'Early'),
-(59, 'QCD027', 027, 'QCD', 6, 4, 1631863331, 'none', '', 'None,image', 'Late', 1631896539, 'Early'),
-(60, 'QCD027', 027, 'QCD', 6, 1, 1631214919, 'none', '', 'None,image', 'Late', 1631250936, 'Over Time'),
-(61, 'STD026', 026, 'STD', 1, 2, 1631493955, 'none', '', 'None,image', 'On Time', 1631523613, 'Over Time'),
-(62, 'ADM011', 011, 'ADM', 1, 1, 1631584873, 'none', '', 'None,image', 'Late', 1631621603, 'Over Time'),
-(63, 'QCD027', 027, 'QCD', 6, 2, 1632109417, 'this is a demo note!', '', 'None,image', 'Late', 1632109437, 'Early'),
-(64, 'ACD002', 002, 'ACD', 2, 3, 1632109840, 'demo demo', '', 'None,image', 'On Time', 1632109845, 'Early'),
-(65, 'STD026', 026, 'STD', 1, 2, 1632109903, 'test', '', 'None,image', 'Late', 1632109905, 'Early'),
 (66, 'ITD028', 028, 'ITD', 7, 7, 1654738570, 'Sample Time-In', 'item-220609-6dc7d7e8fe.png', 'None', 'On Time', 1654738629, 'Early'),
-(71, 'HRD030', 030, 'HRD', 7, 7, 1654756719, 'Sample Note only', 'item-220609-496d1ceffe.png', 'None', 'Late', 0, '');
+(71, 'HRD030', 030, 'HRD', 7, 7, 1654756719, 'Sample Note only', 'item-220609-496d1ceffe.png', 'None', 'Late', 0, ''),
+(72, 'STD031', 031, 'STD', 1, 2, 1673845397, '', '', 'Notes,image', 'Late', 1673845457, 'Early'),
+(73, 'SCD032', 032, 'SCD', 1, 2, 1673858722, '', '', 'Notes,image', 'Late', 1673858841, 'Early');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `department`
+-- Struktur dari tabel `department`
 --
 
 CREATE TABLE `department` (
@@ -75,7 +65,7 @@ CREATE TABLE `department` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `department`
+-- Dumping data untuk tabel `department`
 --
 
 INSERT INTO `department` (`id`, `name`) VALUES
@@ -83,6 +73,7 @@ INSERT INTO `department` (`id`, `name`) VALUES
 ('ADM', 'Admin Department'),
 ('HRD', 'Human Resource Department'),
 ('ITD', 'Information Technology Department - Updated'),
+('OFB', 'Office boy'),
 ('PCD', 'Production Controller Department'),
 ('PLD', 'Planner Department'),
 ('QCD', 'Quality Control Department'),
@@ -92,7 +83,7 @@ INSERT INTO `department` (`id`, `name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `employee`
+-- Struktur dari tabel `employee`
 --
 
 CREATE TABLE `employee` (
@@ -107,19 +98,21 @@ CREATE TABLE `employee` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `employee`
+-- Dumping data untuk tabel `employee`
 --
 
 INSERT INTO `employee` (`id`, `name`, `email`, `gender`, `image`, `birth_date`, `hire_date`, `shift_id`) VALUES
-(001, 'John Smith', 'jsmith@mail.com', 'M', 'default.png', '1996-07-15', '2020-03-05', 7),
+(001, 'Malik Junandar', 'jsmith@mail.com', 'M', 'default.png', '1996-07-15', '2020-03-05', 7),
 (025, 'Admin ', 'admin@admin.com', 'M', 'default.png', '0000-00-00', '0000-00-00', 0),
-(028, 'Mark Cooper', 'mcooper@mail.com', 'M', 'item-220609-64856edd56.png', '1997-06-23', '2021-04-20', 7),
-(030, 'Claire Blake', 'cblake@mail.com', 'F', 'item-220609-97e2f7f0fe.png', '1997-10-14', '2020-03-05', 7);
+(028, 'Ahmad Haryanto', 'mcooper@mail.com', 'M', 'item-220609-64856edd56.png', '1997-06-23', '2021-04-20', 7),
+(030, 'Sinta', 'cblake@mail.com', 'F', 'item-220609-97e2f7f0fe.png', '1997-10-14', '2020-03-05', 7),
+(031, 'Raivan', 'muhamadraivan@gmail.com', 'M', 'default.png', '2002-01-01', '2023-01-16', 7),
+(032, 'Junaidi Saparo', 'junaidi@gmail.com', 'M', 'default.png', '2001-04-24', '2023-01-16', 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `employee_department`
+-- Struktur dari tabel `employee_department`
 --
 
 CREATE TABLE `employee_department` (
@@ -129,31 +122,20 @@ CREATE TABLE `employee_department` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `employee_department`
+-- Dumping data untuk tabel `employee_department`
 --
 
 INSERT INTO `employee_department` (`id`, `employee_id`, `department_id`) VALUES
 (1, 001, 'HRD'),
-(2, 002, 'ACD'),
-(3, 003, 'QCD'),
-(4, 004, 'SCD'),
-(5, 005, 'STD'),
-(6, 006, 'ACD'),
-(7, 007, 'PLD'),
-(8, 008, 'STD'),
-(9, 009, 'STD'),
-(10, 010, 'PCD'),
-(21, 011, 'ADM'),
-(25, 024, 'HRD'),
-(26, 026, 'STD'),
-(27, 027, 'QCD'),
 (28, 028, 'ITD'),
-(30, 030, 'HRD');
+(30, 030, 'HRD'),
+(31, 031, 'ITD'),
+(32, 032, 'SCD');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `location`
+-- Struktur dari tabel `location`
 --
 
 CREATE TABLE `location` (
@@ -162,21 +144,18 @@ CREATE TABLE `location` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `location`
+-- Dumping data untuk tabel `location`
 --
 
 INSERT INTO `location` (`id`, `name`) VALUES
 (1, 'Home'),
 (2, 'Office'),
-(3, 'Store'),
-(4, 'Site'),
-(6, 'Field'),
-(7, 'XYZ Client Office');
+(4, 'Field');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `shift`
+-- Struktur dari tabel `shift`
 --
 
 CREATE TABLE `shift` (
@@ -186,23 +165,22 @@ CREATE TABLE `shift` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `shift`
+-- Dumping data untuk tabel `shift`
 --
 
 INSERT INTO `shift` (`id`, `start`, `end`) VALUES
-(1, '08:00:00', '16:00:00'),
+(1, '08:00:00', '17:00:00'),
 (2, '13:00:00', '21:00:00'),
 (3, '18:00:00', '02:00:00'),
 (4, '03:15:02', '02:05:05'),
 (5, '07:00:00', '18:25:00'),
 (6, '01:00:00', '12:00:00'),
-(7, '09:30:00', '18:30:00'),
-(8, '23:23:23', '23:23:23');
+(7, '09:30:00', '18:30:00');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Struktur dari tabel `users`
 --
 
 CREATE TABLE `users` (
@@ -213,26 +191,21 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `users`
+-- Dumping data untuk tabel `users`
 --
 
 INSERT INTO `users` (`username`, `password`, `employee_id`, `role_id`) VALUES
-('ACD002', '$2y$10$5nv5ehyMVdljfKJ6izsOqOimsbv.cbzU.XLB9ji9zbA.eICdSrNvO', 002, 2),
-('ADM011', '$2y$10$BKpQcs4XKavCcYdFWujzx.Xqb7r9eNkDrOYss2VNXrMJUUpm1agUC', 011, 2),
 ('admin', '$2y$10$tG8hnh7w.yiJL7fjlRCmkuHBwi0QI6DHFXmc1s6X3cAFuw5pRfO/O', 025, 1),
 ('HRD001', '$2y$10$7rLSvRVyTQORapkDOqmkhetjF6H9lJHngr4hJMSM2lHObJbW5EQh6', 001, 2),
-('HRD030', '$2y$10$nGeEuKaPiQ45O6Yvt3BxGuxY.BN.IfMsxP4nWMkM.yXxQkTRyRfDa', 030, 2),
+('HRD030', '$2y$10$Skhhi4kDhqmgZkbtTIWEX.DkFyAtDOmdbXDYrdNRrRzPy83bGRrAG', 030, 2),
 ('ITD028', '$2y$10$vQQ80W50S/rsXThp2YkQ5uH3XKej4bZE68L7Cfz9.MVcQ0EiJo.Hy', 028, 2),
-('PCD010', '$2y$10$BKpQcs4XKavCcYdFWujzx.Xqb7r9eNkDrOYss2VNXrMJUUpm1agUC', 010, 2),
-('QCD027', '$2y$10$peALJo.JKZyD6uMBd41UfuHGQSJe7ExOfDhPITvDbSRRXeWUGY9xy', 027, 2),
-('STD005', '$2y$10$hr35h1fIySFYCSRVL2jRD.RuYa9WtJCEJkkqvQfPboYK7VwURpLim', 005, 2),
-('STD008', '$2y$10$8PGnFaiZPYtcIGrwzMmVZuNKbUb/A88f0NZOA9QVgHaUIJ6ddg.Si', 008, 2),
-('STD026', '$2y$10$8WNMvEEgNPWyRuSeeLDE1uXwnBkYNJE/heLT1zWbsUfYb/wKFyYIy', 026, 2);
+('SCD032', '$2y$10$31EuVe5BUT6guYMDiKu/HOdjxsFY34pmaqtzf8/3S0duNvMVtVaHm', 032, 2),
+('STD031', '$2y$10$8FfJUZJ6TcUXmeHuu6nunuz.B4hppx/BjREzihIplgd89Wk8zJES2', 031, 2);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user_access`
+-- Struktur dari tabel `user_access`
 --
 
 CREATE TABLE `user_access` (
@@ -242,7 +215,7 @@ CREATE TABLE `user_access` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `user_access`
+-- Dumping data untuk tabel `user_access`
 --
 
 INSERT INTO `user_access` (`id`, `role_id`, `menu_id`) VALUES
@@ -255,7 +228,7 @@ INSERT INTO `user_access` (`id`, `role_id`, `menu_id`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user_menu`
+-- Struktur dari tabel `user_menu`
 --
 
 CREATE TABLE `user_menu` (
@@ -264,7 +237,7 @@ CREATE TABLE `user_menu` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `user_menu`
+-- Dumping data untuk tabel `user_menu`
 --
 
 INSERT INTO `user_menu` (`id`, `menu`) VALUES
@@ -277,7 +250,7 @@ INSERT INTO `user_menu` (`id`, `menu`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user_role`
+-- Struktur dari tabel `user_role`
 --
 
 CREATE TABLE `user_role` (
@@ -286,7 +259,7 @@ CREATE TABLE `user_role` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `user_role`
+-- Dumping data untuk tabel `user_role`
 --
 
 INSERT INTO `user_role` (`id`, `name`) VALUES
@@ -296,7 +269,7 @@ INSERT INTO `user_role` (`id`, `name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user_submenu`
+-- Struktur dari tabel `user_submenu`
 --
 
 CREATE TABLE `user_submenu` (
@@ -309,7 +282,7 @@ CREATE TABLE `user_submenu` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `user_submenu`
+-- Dumping data untuk tabel `user_submenu`
 --
 
 INSERT INTO `user_submenu` (`id`, `menu_id`, `title`, `url`, `icon`, `is_active`) VALUES
@@ -330,7 +303,7 @@ INSERT INTO `user_submenu` (`id`, `menu_id`, `title`, `url`, `icon`, `is_active`
 --
 
 --
--- Indexes for table `attendance`
+-- Indeks untuk tabel `attendance`
 --
 ALTER TABLE `attendance`
   ADD PRIMARY KEY (`id`),
@@ -341,20 +314,20 @@ ALTER TABLE `attendance`
   ADD KEY `location_id` (`location_id`);
 
 --
--- Indexes for table `department`
+-- Indeks untuk tabel `department`
 --
 ALTER TABLE `department`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `employee`
+-- Indeks untuk tabel `employee`
 --
 ALTER TABLE `employee`
   ADD PRIMARY KEY (`id`),
   ADD KEY `shift_id_fk_e` (`shift_id`);
 
 --
--- Indexes for table `employee_department`
+-- Indeks untuk tabel `employee_department`
 --
 ALTER TABLE `employee_department`
   ADD PRIMARY KEY (`id`),
@@ -362,19 +335,19 @@ ALTER TABLE `employee_department`
   ADD KEY `employee_department_ibfk_2` (`department_id`);
 
 --
--- Indexes for table `location`
+-- Indeks untuk tabel `location`
 --
 ALTER TABLE `location`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `shift`
+-- Indeks untuk tabel `shift`
 --
 ALTER TABLE `shift`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `users`
+-- Indeks untuk tabel `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`username`),
@@ -382,7 +355,7 @@ ALTER TABLE `users`
   ADD KEY `role_id` (`role_id`);
 
 --
--- Indexes for table `user_access`
+-- Indeks untuk tabel `user_access`
 --
 ALTER TABLE `user_access`
   ADD PRIMARY KEY (`id`),
@@ -390,126 +363,100 @@ ALTER TABLE `user_access`
   ADD KEY `role_id` (`role_id`);
 
 --
--- Indexes for table `user_menu`
+-- Indeks untuk tabel `user_menu`
 --
 ALTER TABLE `user_menu`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `user_role`
+-- Indeks untuk tabel `user_role`
 --
 ALTER TABLE `user_role`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `user_submenu`
+-- Indeks untuk tabel `user_submenu`
 --
 ALTER TABLE `user_submenu`
   ADD PRIMARY KEY (`id`),
   ADD KEY `menu_id` (`menu_id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `attendance`
+-- AUTO_INCREMENT untuk tabel `attendance`
 --
 ALTER TABLE `attendance`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=72;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
 
 --
--- AUTO_INCREMENT for table `employee`
+-- AUTO_INCREMENT untuk tabel `employee`
 --
 ALTER TABLE `employee`
-  MODIFY `id` int(3) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `id` int(3) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
--- AUTO_INCREMENT for table `employee_department`
+-- AUTO_INCREMENT untuk tabel `employee_department`
 --
 ALTER TABLE `employee_department`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
--- AUTO_INCREMENT for table `location`
+-- AUTO_INCREMENT untuk tabel `location`
 --
 ALTER TABLE `location`
-  MODIFY `id` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `shift`
+-- AUTO_INCREMENT untuk tabel `shift`
 --
 ALTER TABLE `shift`
-  MODIFY `id` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT for table `user_access`
+-- AUTO_INCREMENT untuk tabel `user_access`
 --
 ALTER TABLE `user_access`
   MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT for table `user_menu`
+-- AUTO_INCREMENT untuk tabel `user_menu`
 --
 ALTER TABLE `user_menu`
   MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT for table `user_role`
+-- AUTO_INCREMENT untuk tabel `user_role`
 --
 ALTER TABLE `user_role`
   MODIFY `id` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `user_submenu`
+-- AUTO_INCREMENT untuk tabel `user_submenu`
 --
 ALTER TABLE `user_submenu`
   MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
--- Constraints for dumped tables
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Constraints for table `attendance`
---
-ALTER TABLE `attendance`
-  ADD CONSTRAINT `attendance_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `attendance_ibfk_2` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `attendance_ibfk_3` FOREIGN KEY (`department_id`) REFERENCES `department` (`id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `attendance_ibfk_4` FOREIGN KEY (`shift_id`) REFERENCES `shift` (`id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `attendance_ibfk_5` FOREIGN KEY (`location_id`) REFERENCES `location` (`id`) ON UPDATE CASCADE;
-
---
--- Constraints for table `employee`
---
-ALTER TABLE `employee`
-  ADD CONSTRAINT `shift_id_fk_e` FOREIGN KEY (`shift_id`) REFERENCES `shift` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
-
---
--- Constraints for table `employee_department`
---
-ALTER TABLE `employee_department`
-  ADD CONSTRAINT `employee_department_ibfk_1` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `employee_department_ibfk_2` FOREIGN KEY (`department_id`) REFERENCES `department` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `users`
---
-ALTER TABLE `users`
-  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `users_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `user_role` (`id`) ON UPDATE CASCADE;
-
---
--- Constraints for table `user_access`
+-- Ketidakleluasaan untuk tabel `user_access`
 --
 ALTER TABLE `user_access`
   ADD CONSTRAINT `user_access_ibfk_1` FOREIGN KEY (`menu_id`) REFERENCES `user_menu` (`id`) ON UPDATE CASCADE,
   ADD CONSTRAINT `user_access_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `user_role` (`id`) ON UPDATE CASCADE;
 
 --
--- Constraints for table `user_submenu`
+-- Ketidakleluasaan untuk tabel `user_submenu`
 --
 ALTER TABLE `user_submenu`
   ADD CONSTRAINT `user_submenu_ibfk_1` FOREIGN KEY (`menu_id`) REFERENCES `user_menu` (`id`) ON UPDATE CASCADE;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
